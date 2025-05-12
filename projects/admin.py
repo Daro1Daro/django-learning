e86d95a467b567a9fbb3780135b2d6762e2653cb
dmalagowski <dariusz.malagowski@stxnext.pl>
2025-05-12 14:33:52 +0200

    add project model

---
+++ projects/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "created_at")
+    search_fields = ("name", "owner__username", "members__username")
+    list_filter = ("created_at",)
+    filter_horizontal = ("members",)
