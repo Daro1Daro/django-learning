1c04436e028214363445167135289f6df0e6bf88
dmalagowski <dariusz.malagowski@stxnext.pl>
2025-05-23 14:07:43 +0200

    update api and commands

--- projects/api.py
+++ projects/api.py
@@ -3,10 +3,16 @@ from django.urls import reverse
 from ninja import Router, Schema, Query, FilterSchema, Field
 from typing import List, Optional
 from datetime import datetime
+from pydantic import FutureDatetime
 
-from .models import Project
-from .queries import query_get_user_projects, query_get_project
-from .commands import command_create_project, command_update_project
+from .queries import query_get_user_projects, query_get_project, query_get_task
+from .commands import (
+    command_create_project,
+    command_update_project,
+    command_delete_project,
+    command_create_task,
+)
+from .models import StatusChoice
 
 router = Router()
 
@@ -16,7 +22,15 @@ class ProjectInput(Schema):
     member_ids: List[int]
 
 
-class MemberOutput(Schema):
+class TaskInput(Schema):
+    title: str = Field(max_length=255)
+    description: str = Field(max_length=10000)
+    status: StatusChoice
+    assignee_id: Optional[int]
+    due_date: Optional[FutureDatetime]
+
+
+class UserOutput(Schema):
     id: int
     email: str
 
@@ -25,7 +39,17 @@ class ProjectOutput(Schema):
     id: int
     name: str
     owner_id: int
-    members: List[MemberOutput]
+    members: List[UserOutput]
+    created_at: datetime
+
+
+class TaskOutput(Schema):
+    title: str
+    description: str
+    status: StatusChoice
+    assignee: Optional[UserOutput]
+    due_date: Optional[datetime]
+    created_by: UserOutput
     created_at: datetime
 
 
@@ -39,7 +63,7 @@ class ProjectFilterSchema(FilterSchema):
 def get_projects(request: HttpRequest, filters: ProjectFilterSchema = Query(...)):
     user = request.auth["user"]
 
-    if len(request.GET.keys()) == 0:
+    if not request.GET:
         projects = query_get_user_projects(user=user)
     else:
         projects = filters.filter(query_get_user_projects(user=user))
@@ -67,7 +91,7 @@ def create_project(request: HttpRequest, response: HttpResponse, payload: Projec
 
 @router.patch("/{id}", response=ProjectOutput)
 def update_project(
-    request: HttpRequest, id: int, response: HttpResponse, payload: ProjectInput
+    request: HttpRequest, response: HttpResponse, id: int, payload: ProjectInput
 ):
     user = request.auth["user"]
     project = command_update_project(user=user, project_id=id, **payload.dict())
@@ -77,3 +101,33 @@ def update_project(
     )
 
     return project
+
+
+@router.delete("/{id}")
+def delete_project(request: HttpRequest, response: HttpResponse, id: int):
+    user = request.auth["user"]
+    command_delete_project(user=user, project_id=id)
+
+    response.status_code = 203
+
+    return response
+
+
+@router.get("/tasks/{id}", url_name="get_task", response=TaskOutput)
+def get_task(request: HttpRequest, id: int):
+    user = request.auth["user"]
+    return query_get_task(user=user, task_id=id)
+
+
+@router.post("/{project_id}/tasks", response=TaskOutput)
+def create_task(
+    request: HttpRequest, response: HttpResponse, project_id: int, payload: TaskInput
+):
+    user = request.auth["user"]
+    task = command_create_task(user=user, project_id=project_id, **payload.dict())
+
+    response["Location"] = request.build_absolute_uri(
+        reverse("api-1:get_task", args=[task.id])
+    )
+
+    return task
--- projects/commands.py
+++ projects/commands.py
@@ -1,9 +1,11 @@
-from typing import List
+from typing import List, Optional
 from guardian.shortcuts import assign_perm
+from datetime import datetime
 
 
 from users.models import User
-from .models import Project
+from users.queries import query_get_user_by_id
+from .models import Project, Task, StatusChoice
 from .queries import query_get_project
 from .permissions import Permissions
 from .exceptions import ProjectPermissionDenied
@@ -12,12 +14,14 @@ from .exceptions import ProjectPermissionDenied
 def command_create_project(user: User, name: str, member_ids: List[int]) -> Project:
     project: Project = Project.objects.create(owner=user, name=name)
 
+    # TODO: check if members exist
     members = User.objects.filter(id__in=member_ids)
     project.members.set(members)
 
     assign_perm(Permissions.DELETE_PROJECT, user, project)
     assign_perm(Permissions.VIEW_PROJECT, user, project)
     assign_perm(Permissions.UPDATE_PROJECT, user, project)
+    assign_perm(Permissions.CREATE_TASK, user, project)
 
     for member in members:
         assign_perm(Permissions.VIEW_PROJECT, member, project)
@@ -28,9 +32,9 @@ def command_create_project(user: User, name: str, member_ids: List[int]) -> Proj
 def command_update_project(
     user: User, project_id: int, name: str, member_ids: List[int]
 ) -> Project:
-    project: Project = query_get_project(uid=user.id, id=project_id)
+    project: Project = query_get_project(user=user, project_id=project_id)
 
-    if not user.has_perm(Permissions.UPDATE_PROJECT, project):
+    if not user.has_perm(perm=Permissions.UPDATE_PROJECT, obj=project):
         raise ProjectPermissionDenied
 
     project.name = name
@@ -39,6 +43,59 @@ def command_update_project(
     for member in members:
         assign_perm(Permissions.VIEW_PROJECT, member, project)
 
+    project.full_clean()
     project.save()
 
     return project
+
+
+def command_delete_project(user: User, project_id: int):
+    project: Project = query_get_project(user=user, project_id=project_id)
+
+    if not user.has_perm(perm=Permissions.DELETE_PROJECT, obj=project):
+        raise ProjectPermissionDenied
+
+    project.delete()
+
+
+def command_create_task(
+    user: User,
+    project_id: int,
+    title: str,
+    description: str,
+    status: StatusChoice,
+    assignee_id: Optional[int] = None,
+    due_date: Optional[datetime] = None,
+) -> Task:
+    project: Project = query_get_project(user=user, project_id=project_id)
+
+    if not user.has_perm(perm=Permissions.CREATE_TASK, obj=project):
+        raise ProjectPermissionDenied
+
+    if assignee_id:
+        assignee = query_get_user_by_id(uid=assignee_id)
+
+    print(assignee)
+
+    task = Task(
+        created_by=user,
+        project=project,
+        title=title,
+        description=description,
+        status=status,
+        assignee=assignee,
+        due_date=due_date,
+    )
+
+    task.full_clean()
+    task.save()
+
+    assign_perm(Permissions.DELETE_TASK, user, task)
+    assign_perm(Permissions.VIEW_TASK, user, task)
+    assign_perm(Permissions.UPDATE_TASK, user, task)
+
+    if assignee:
+        assign_perm(Permissions.VIEW_TASK, assignee, task)
+        assign_perm(Permissions.UPDATE_TASK, assignee, task)
+
+    return task
