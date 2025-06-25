3dab5a0776c7c1a798d23c7591f8c09eae36dc9e
dmalagowski <dariusz.malagowski@stxnext.pl>
2025-06-25 17:58:40 +0200

    add typing

--- backend/projects/queries.py
+++ backend/projects/queries.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.shortcuts import get_object_or_404
 from guardian.shortcuts import get_objects_for_user
 
@@ -35,5 +37,5 @@ def query_get_task(user: User, task_id: int) -> Task:
     return task
 
 
-def query_get_tasks(user: User):
+def query_get_tasks(user: User) -> List[Task]:
     return get_objects_for_user(user=user, perms=Permissions.VIEW, klass=Task)
